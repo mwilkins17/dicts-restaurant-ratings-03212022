5c76417a846e87bc73c20906d31a82ccbea09f78
Mason Wilkins <mwilkins17@gmail.com>
2022-03-21 20:35:08 -0400

    Fnal Commit

--- ratings.py
+++ ratings.py
@@ -19,28 +19,28 @@ def get_restaurant_and_ratings(file_name):
     
     
     restaurant_input = input("What restaurant would you like to rate?: ")
-    rating_input = input("What rating would you like to give?(1-5): ")
-    # rating_input = ":" + input("What rating would you like to give?(1-5): ")
-    # restaurant_and_rating_input = restaurant_input + rating_input
-    # sorted_file = [] + restaurant_and_rating_input
-    sorted_file = sorted(file)
+    # rating_input = input("What rating would you like to give?(1-5): ")
+    rating_input = ":" + input("What rating would you like to give?(1-5): ")
+    restaurant_and_rating_input = [restaurant_input + rating_input]
 
-    
+    sort_file = sorted(file) + restaurant_and_rating_input
+    sorted_file = sorted(sort_file)
     # print(sorted_file)
+    
     for line in sorted_file:
         line = line.rstrip().split(":") # order matters! 
         restaurant = line[0]
         rating = line[1]
         restaurants_and_ratings[restaurant] = restaurants_and_ratings.get(restaurant, 0) + int(rating)
 
-        #if restaurants_and_ratings.get(restaurant_input, rating_input) == 0:
-            #restaurants_and_ratings[restaurant_input] = rating_input
 
-    restaurants_and_ratings[restaurant_input] = restaurants_and_ratings.get(restaurant_input, int(rating_input)) 
+    # restaurants_and_ratings[restaurant_input] = restaurants_and_ratings.get(restaurant_input, int(rating_input)) 
+    
 
     
     for key, value in restaurants_and_ratings.items():
         print(f'{key} is rated at {value}.')
+        
     file = file.close()
     
     
