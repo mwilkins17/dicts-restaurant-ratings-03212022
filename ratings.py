75dafda0adbb70d5a7c0b06c5298320f24538839
Mason Wilkins <mwilkins17@gmail.com>
2022-03-21 20:22:37 -0400

    user input added. Still need to sort user input alphabetically

--- ratings.py
+++ ratings.py
@@ -4,20 +4,45 @@
 #stores the info in a dictionary, and spits them
 #alphabetical order
 
+# prompt the user for a restaurant name
+# prompt for restaurant score
+# store new restaurant + ratings in dictionary 
+# print all in alph order 
+
+from re import S
+
+
 def get_restaurant_and_ratings(file_name):
     file = open(file_name)
     restaurants_and_ratings = {}
+    
+    
+    
+    restaurant_input = input("What restaurant would you like to rate?: ")
+    rating_input = input("What rating would you like to give?(1-5): ")
+    # rating_input = ":" + input("What rating would you like to give?(1-5): ")
+    # restaurant_and_rating_input = restaurant_input + rating_input
+    # sorted_file = [] + restaurant_and_rating_input
     sorted_file = sorted(file)
+
+    
     # print(sorted_file)
     for line in sorted_file:
         line = line.rstrip().split(":") # order matters! 
         restaurant = line[0]
         rating = line[1]
         restaurants_and_ratings[restaurant] = restaurants_and_ratings.get(restaurant, 0) + int(rating)
-  
+
+        #if restaurants_and_ratings.get(restaurant_input, rating_input) == 0:
+            #restaurants_and_ratings[restaurant_input] = rating_input
+
+    restaurants_and_ratings[restaurant_input] = restaurants_and_ratings.get(restaurant_input, int(rating_input)) 
+
     
     for key, value in restaurants_and_ratings.items():
         print(f'{key} is rated at {value}.')
     file = file.close()
+    
+    
 
 get_restaurant_and_ratings("scores.txt")
